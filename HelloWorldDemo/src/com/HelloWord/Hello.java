2b51d963dd8c3722e4b9bb3bbb3fc4f05b041f3a
ylei1 <109705861+ylei1@users.noreply.github.com>
2022-08-04 12:42:38 +0800

    Add TestDemo by using github

---
+++ HelloWorldDemo/src/com/HelloWord/Hello.java
@@ -0,0 +1,21 @@
+package com.HelloWord;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.text.*;
+import java.util.*;
+
+
+
+public class Hello {
+
+    public static Set<String> languageSet = new HashSet<String>();
+
+    public static void main(String[] args) {
+
+        languageSet.add("en");
+        languageSet.add("cn");
+    }
+}
+
+
