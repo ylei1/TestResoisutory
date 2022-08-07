b04385e80fbb8b9c0809d0823e7ad41ca9bef7e9
ylei1 <109705861+ylei1@users.noreply.github.com>
2022-08-07 22:23:26 +0800

    1

---
+++ SpringMVCDemo/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+}
